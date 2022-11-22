c225cdefb6721b388ab38e7b85949eec888a2639
andrew <agatinet@gmail.com>
2022-11-22 15:50:23 +0300

    src init

---
+++ src/constants.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+MAIN_DOC_URL = 'https://docs.python.org/3/'
+
+DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+
+BASE_DIR = Path(__file__).parent
---
+++ src/main.py
@@ -0,0 +1,131 @@
+# main.py
+import logging
+import re
+from urllib.parse import urljoin
+
+import requests_cache
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+from configs import configure_argument_parser, configure_logging
+from constants import BASE_DIR, MAIN_DOC_URL
+from outputs import control_output
+from utils import find_tag, get_response
+
+
+def whats_new(session):
+    whats_new_url = urljoin(MAIN_DOC_URL, 'whatsnew/')
+    response = get_response(session, whats_new_url)
+    if response is None:
+        return
+    soup = BeautifulSoup(response.text, features='lxml')
+    main_div = find_tag(soup, 'section', attrs={'id': 'what-s-new-in-python'})
+    div_with_ul = find_tag(main_div, 'div', attrs={'class': 'toctree-wrapper'})
+    sections_by_python = div_with_ul.find_all(
+            'li', attrs={'class': 'toctree-l1'}
+        )
+    results = [('Ссылка на документацию', 'Версия', 'Статус')]
+    for section in tqdm(sections_by_python):
+        version_a_tag = find_tag(section, 'a')
+        href = version_a_tag['href']
+        version_link = urljoin(whats_new_url, href)
+        response = get_response(session, version_link)
+        if response is None:
+            continue
+        soup = BeautifulSoup(response.text, 'lxml')
+        h1 = find_tag(soup, 'h1')
+        dl = find_tag(soup, 'dl')
+        dl_text = dl.text.replace('\n', ' ')
+        results.append(
+            (version_link, h1.text, dl_text)
+        )
+    return results
+
+
+def latest_versions(session):
+    response = get_response(session, MAIN_DOC_URL)
+    if response is None:
+        return
+    soup = BeautifulSoup(response.text, 'lxml')
+    sidebar = find_tag(soup, 'div', {'class': 'sphinxsidebarwrapper'})
+    ul_tags = sidebar.find_all('ul')
+    for ul in ul_tags:
+        if 'All versions' in ul.text:
+            a_tags = ul.find_all('a')
+            break
+    else:
+        raise Exception('Ничего не нашлось')
+    results = [('Ссылка на документацию', 'Версия', 'Статус')]
+    pattern = r'Python (?P<version>\d\.\d+) \((?P<status>.*)\)'
+    for a_tag in a_tags:
+        link = a_tag['href']
+        text_match = re.search(pattern, a_tag.text)
+        if text_match is not None:
+            version, status = text_match.groups()
+        else:
+            version, status = a_tag.text, ''
+        results.append(
+            (link, version, status)
+        )
+    return results
+
+
+def download(session):
+    downloads_url = urljoin(MAIN_DOC_URL, 'download.html')
+    response = get_response(session, downloads_url)
+    if response is None:
+        return
+    soup = BeautifulSoup(response.text, 'lxml')
+    download_table = find_tag(soup, 'table', {'class': 'docutils'})
+    if download_table is None:
+        raise Exception('Ссылки для загрузки не распарсены!')
+    # r'.+pdf-a4\.zip$'
+    pattern = r'href=\"(?P<url>.*pdf-a4.zip)\"'
+    download_match = re.search(pattern, str(download_table))
+    if download_match is None:
+        raise Exception('Ссылка на скачивание A4 (pdf, zip) отсутствует!')
+    href = download_match.group('url')
+
+    # pdf_a4_tag = table_tag.find('a', {'href': re.compile(r'.+pdf-a4\.zip$')})
+    # pdf_a4_link = pdf_a4_tag['href']
+
+    a4_pdf_zip_link = urljoin(downloads_url, href)
+    print(a4_pdf_zip_link)
+    downloads_dir = BASE_DIR / 'downloads'
+    downloads_dir.mkdir(exist_ok=True)
+    filename = a4_pdf_zip_link.split('/')[-1]
+    archive_path = downloads_dir / filename
+    # Загрузка архива по ссылке.
+    response = session.get(a4_pdf_zip_link)
+    # В бинарном режиме открывается файл на запись по указанному пути.
+    with open(archive_path, 'wb') as file:
+        # Полученный ответ записывается в файл.
+        file.write(response.content)
+    logging.info(f'Архив был загружен и сохранён: {archive_path}')
+
+
+MODE_TO_FUNCTION = {
+    'whats-new': whats_new,
+    'latest-versions': latest_versions,
+    'download': download,
+}
+
+
+def main():
+    configure_logging()
+    logging.info('Парсер запущен!')
+    arg_parser = configure_argument_parser(MODE_TO_FUNCTION.keys())
+    args = arg_parser.parse_args()
+    logging.info(f'Аргументы командной строки: {args}')
+    session = requests_cache.CachedSession()
+    if args.clear_cache:
+        session.cache.clear()
+    parser_mode = args.mode
+    results = MODE_TO_FUNCTION[parser_mode](session)
+    if results is not None:
+        control_output(results, args)
+    logging.info('Парсер завершил работу.')
+
+
+if __name__ == '__main__':
+    main()
